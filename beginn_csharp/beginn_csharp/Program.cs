e30bb09c104ed4adb5f6fd0967c8932a111eb473
Schaeberle, Erwin <erwin.schaeberle@tecalliance.net>
2022-09-15 09:25:36 +0200

    Age ask for the user #3

--- beginn_csharp/beginn_csharp/Program.cs
+++ beginn_csharp/beginn_csharp/Program.cs
@@ -14,7 +14,14 @@ namespace beginn_csharp
             string username = Console.ReadLine();
 //            string name = "Erwin";
             Console.WriteLine("hello Wold! " + username);
+            Console.WriteLine("Wie alt bist du? ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dein Alter ist " + age);
+
             Console.ReadLine();
+
+            
+
 
         }
     }
